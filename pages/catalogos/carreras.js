935b7e6ecc99c04e2b852648ed0f16cf4133edaf
Carlos Guzman <103605640+CGuzman99@users.noreply.github.com>
2023-08-02 00:19:17 +0000

    Update 4 files

--- pages/catalogos/carreras.js
+++ pages/catalogos/carreras.js
@@ -1,66 +1,75 @@
-import Link from 'next/link';
-import styles from './catalogos.module.css';
-import { useState } from 'react';
-import Layout from '@/components/layout';
-import {BsArrowUpRight} from 'react-icons/bs';
-import { ImArrowUpRight2 } from 'react-icons/im';
-import { createClient } from 'contentful';
-import Image from 'next/image';
+import Link from "next/link";
+import styles from "./catalogos.module.css";
+import { useState } from "react";
+import Layout from "@/components/layout";
+import { BsArrowUpRight } from "react-icons/bs";
+import { ImArrowUpRight2 } from "react-icons/im";
+import { createClient } from "contentful";
+import Image from "next/image";
+import Navbar from "@/components/navbar";
 
 export const getStaticProps = async () => {
   const client = createClient({
     space: process.env.SPACE_ID,
-    accessToken: process.env.ACCESS_TOKEN
-  })
+    accessToken: process.env.ACCESS_TOKEN,
+  });
 
-  let data = await client.getEntries({content_type: "catalogo"})
+  let data = await client.getEntries({ content_type: "catalogo" });
   return {
     props: {
-      games: data
-    }
-  }
-}
+      games: data,
+    },
+  };
+};
 
-export default function Catalogo({games}) {
-  console.log(games)
+export default function Catalogo({ games }) {
+  console.log(games);
   let img_url = "https:" + games.items[0].fields.media[0].fields.file.url;
-  let img_width = games.items[0].fields.media[0].fields.file.details.image.width; 
-  let img_height = games.items[0].fields.media[0].fields.file.details.image.height;
+  let img_width =
+    games.items[0].fields.media[0].fields.file.details.image.width;
+  let img_height =
+    games.items[0].fields.media[0].fields.file.details.image.height;
   return (
     <Layout>
       <header>
         <HeaderCard />
       </header>
       <main className={styles.main}>
-        <Image src={img_url} width={200} height={200}/>
+        <Image src={img_url} width={200} height={200} />
         {JSON.stringify(games)}
       </main>
     </Layout>
-  )
+  );
 }
 
-function HeaderCard(){
+function HeaderCard() {
   return (
     <>
+      <Navbar />
       <div className={styles.apply_variable}>
         <h1>Carreras</h1>
-        <SearchBar find={() => {}}/>
-        <Link href={'/realidadVirtual'}><span className={styles.arrow_icon}><ImArrowUpRight2/></span>realidad virtual</Link>
+        <SearchBar find={() => {}} />
+        <Link href={"/realidadVirtual"}>
+          <span className={styles.arrow_icon}>
+            <ImArrowUpRight2 />
+          </span>
+          realidad virtual
+        </Link>
       </div>
     </>
   );
 }
 
-function SearchBar({find}){
-  const [input, setInput] = useState('');
+function SearchBar({ find }) {
+  const [input, setInput] = useState("");
 
-  function handleInput(event){
+  function handleInput(event) {
     find(event.target.value);
     setInput(event.target.value);
   }
   return (
     <>
-      <input placeholder='buscar...' value={input} onChange={handleInput}/>
+      <input placeholder="buscar..." value={input} onChange={handleInput} />
     </>
   );
 }
