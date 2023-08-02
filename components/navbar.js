9d3423067c42a6b8c255aed69dcd50e55ee2a238
Carlos Guzman <103605640+CGuzman99@users.noreply.github.com>
2023-08-02 00:21:01 +0000

    Update navbar.js

--- components/navbar.js
+++ components/navbar.js
@@ -1,15 +1,20 @@
 import styles from "./navbar.module.css";
 
-export default function Navbar ({ }) {
-    return (
-        <nav className={styles.apply_variable} class="navbar navbar-expand-sm bg-light navbar-light">
-            <div class="container-fluid">
-                <ul class="navbar-nav">
-                    <li class="nav-item">
-                        <a class="nav-link active" href="#">Inicio</a>
-                    </li>
-                </ul>
-            </div> 
-        </nav>
-    );
+export default function Navbar({}) {
+  return (
+    <nav
+      className={styles.apply_variable}
+      class="navbar navbar-expand-sm bg-light navbar-light"
+    >
+      <div class="container-fluid">
+        <ul class="navbar-nav">
+          <li class="nav-item">
+            <a class="nav-link active" href="#">
+              Inicio
+            </a>
+          </li>
+        </ul>
+      </div>
+    </nav>
+  );
 }
