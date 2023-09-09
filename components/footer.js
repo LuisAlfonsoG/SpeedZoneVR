f128799580b4b03ab8d2fe1ac4efd5c09fcf8a36
Luis <luisalfonso625@gmail.com>
2023-09-09 16:52:06 -0600

    Added link

--- components/footer.js
+++ components/footer.js
@@ -1,6 +1,6 @@
 import styles from "./footer.module.css";
 
-export default function Footer({}) {
+export default function Footer({ }) {
   return (
     <>
       <div
@@ -15,15 +15,17 @@ export default function Footer({}) {
                 <i className={`${styles.icon} bi bi-geo-alt-fill`}></i>
               </span>
               <div className={`align-self-center`}>
-                Av. de la Cantera 2631-Planta alta Local I, 
+                Av. de la Cantera 2631-Planta alta Local I,
                 Ciudad del sol, 76116 Santiago de Querétaro, Qro.
               </div>
             </div>
             <div className={`d-flex mt-2`}>
-              <span className={`me-2`}>
-                <i className={`${styles.icon} bi bi-whatsapp`}></i>
-              </span>
-              <div className={`align-self-center`}>+52 442 113 6186</div>
+              <a className={styles.social_links} href="https://wa.me/+524421136186">
+                <span className={`me-2`}>
+                  <i className={`${styles.icon} bi bi-whatsapp`}></i>
+                </span>
+                <div className={`align-self-center`}>+52 442 113 6186</div>
+              </a>
             </div>
             <div className={`d-flex mt-2`}>
               <span className={`me-2`}>
@@ -52,13 +54,13 @@ export default function Footer({}) {
           </div>
           <div className={`col justify-content-center mt-3 me-2`}>
             <div className={`d-flex justify-content-center`}>
-              <iframe 
-                src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3733.743929605549!2d-100.49298352485789!3d20.639291880914428!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x85d3510cbb0ce233%3A0x5bee090713897d85!2sSpeed%20Zone%20VR!5e0!3m2!1ses-419!2smx!4v1694116013716!5m2!1ses-419!2smx" 
-                width="600" 
-                height="450" 
-                style={{border: 0}} 
-                allowfullscreen="" 
-                loading="lazy" 
+              <iframe
+                src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3733.743929605549!2d-100.49298352485789!3d20.639291880914428!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x85d3510cbb0ce233%3A0x5bee090713897d85!2sSpeed%20Zone%20VR!5e0!3m2!1ses-419!2smx!4v1694116013716!5m2!1ses-419!2smx"
+                width="600"
+                height="450"
+                style={{ border: 0 }}
+                allowfullscreen=""
+                loading="lazy"
                 referrerpolicy="no-referrer-when-downgrade"
               ></iframe>
             </div>
